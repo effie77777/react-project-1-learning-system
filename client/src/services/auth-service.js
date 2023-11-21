39fd05761208c2ff8df869b139b8e9447eaeefea
您的姓名 <effie0821@gmail.com>
2023-11-21 11:48:25 +0800

    update

--- client/src/services/auth-service.js
+++ client/src/services/auth-service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const API_URL = "https://react-project-1-learning-system.onrender.com";
 
+console.log(API_URL);
 class AuthService {
     login(email, password) {
         return axios.post(
